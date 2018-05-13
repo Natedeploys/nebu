b715b911f9becc7e808f2aa9485a9f3da8d94ec3
Jonathan Prada <jonathanprada@protonmail.ch>
2018-05-13 18:41:28 +0200

    fixed bugs and added profile routing

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get('/test', (req, res) => {
-  res.json({
-    msg: "profile works"
-  });
+// Load profile model 
+const Profile = require('../../models/Profile');
+// Load user profile 
+const User = require('../../models/User');
+
+// @route:       GET api/profile
+// @description: Get current users profile
+// @access:      Private
+
+// Protect route using passport.authenticate and jwt
+router.get('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ // find a profile based on user id
+      user: req.user.id
+    })
+    .then(profile => {
+      if (!profile) { // If there is no profile
+        errors.noprofile = "There is no profile for this user";
+        return res.status(400).json(errors);
+      }
+      // If there is a profile, send 200 response with profile
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
--- validation/register.js
+++ validation/register.js
@@ -4,10 +4,10 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  data.name = isEmpty(data.name) ? data.name : '';
-  data.email = isEmpty(data.email) ? data.email : '';
-  data.password = isEmpty(data.password) ? data.password : '';
-  data.password2 = isEmpty(data.password2) ? data.password2 : '';
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   // If name is not empty but less than 2 or over 30 chars
   if (!Validator.isLength(data.name, {
@@ -34,7 +34,7 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password field is required"
   }
 
-  if (Validator.isLength(data.password, {
+  if (!Validator.isLength(data.password, {
       min: 6,
       max: 30
     })) {
@@ -45,7 +45,7 @@ module.exports = function validateRegisterInput(data) {
     errors.password2 = "Confirm Password field is required"
   }
 
-  if (Validator.equals(data.password, data.password2)) {
+  if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match"
   }
 
